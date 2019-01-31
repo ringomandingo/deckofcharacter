81f9f868e4e32beee2d5b61d82aff7155ce9e0f0
Willie Maglothin <ringomann@gmail.com>
2019-01-31 12:49:14 -0700

    Added files

---
+++ precache-manifest.91dea41275a7814f7d041b05bd7c3f39.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a2cf294288f6d19f86cf",
+    "url": "/deckofcharacter/static/css/main.1d2975ca.chunk.css"
+  },
+  {
+    "revision": "a2cf294288f6d19f86cf",
+    "url": "/deckofcharacter/static/js/main.a2cf2942.chunk.js"
+  },
+  {
+    "revision": "af971c4aac594368907a",
+    "url": "/deckofcharacter/static/js/1.af971c4a.chunk.js"
+  },
+  {
+    "revision": "55c6c2b99622af343a5f",
+    "url": "/deckofcharacter/static/js/runtime~main.55c6c2b9.js"
+  },
+  {
+    "revision": "df859a8fd8ec1ca8c17e42edb49e1714",
+    "url": "/deckofcharacter/static/media/open2.df859a8f.mp3"
+  },
+  {
+    "revision": "490a5ba15b96016b2dd5f7c2182662ac",
+    "url": "/deckofcharacter/static/media/closed5.490a5ba1.mp3"
+  },
+  {
+    "revision": "009f6d9dffea262aeafbff59cc31f8b4",
+    "url": "/deckofcharacter/static/media/popup3.009f6d9d.mp3"
+  },
+  {
+    "revision": "0a60322a83da1adb8e066e319e20ff38",
+    "url": "/deckofcharacter/index.html"
+  }
+];
